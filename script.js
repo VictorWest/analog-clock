f0fc31c84a438ae792c16d327bcd92701bc5fe37
VictorWest <victorwaribokowest@gmail.com>
2023-11-14 15:37:34 +0100

    finish the hour hand

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ setInterval(() => {
     secondHand.style.transform = `rotate(${secondChange}deg)`
 }, 1000)
 setInterval(() => {
-    let minuteChange = (((new Date).getMinutes())/60)*360
-    // console.log(minuteChange)
+    let secondChange = ((new Date).getSeconds()/60)
+    let minuteChange = ((secondChange + (new Date).getMinutes())/60)*360
     minuteHand.style.transform = `rotate(${minuteChange}deg)`
 }, 1000)
-
-var hourChange = (((new Date).getHours()/12)*360)
 setInterval(() => {
-    hourChange += 0.5
-    console.log(hourChange)
+    let secondChange = ((new Date).getSeconds()/60)
+    let minuteChange = (secondChange + (new Date).getMinutes())/60
+    let hourChange = ((minuteChange + ((new Date).getHours()))/12)*360
+    console.log(hourChange);
     hourHand.style.transform = `rotate(${hourChange}deg)`
 }, 1000)
